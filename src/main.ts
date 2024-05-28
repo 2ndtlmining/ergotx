c0e4b04fc8c3683c78b398f6e86625127833d00f
enix403 <fog.code000@gmail.com>
2024-05-28 22:09:43 +0500

    phaser

--- src/main.ts
+++ src/main.ts
@@ -7,43 +7,85 @@ import { Transaction } from "./types";
 
 const { Rectangle } = Geom;
 
-class Person {
-
-}
+class Person {}
 
 class MainScene extends Phaser.Scene {
-  private updateService!: UpdateService;
+  private updateService: UpdateService;
   private mempool: Transaction[] = [];
 
+  private speed = 100;
+  private personBody: Phaser.Physics.Arcade.Body;
+
+  private start = new Phaser.Math.Vector2(50, 400);
+  private dest = new Phaser.Math.Vector2(400, 300);
+
+  private distanceText: Phaser.GameObjects.Text;
+
   init() {
-    this.updateService = new UpdateService().onUnconfirmedTransactions(
-      this.handleUnconfirmedTransactions
-    );
-
-    this.updateService.start();
+    // this.updateService = new UpdateService().onUnconfirmedTransactions(
+    // this.handleUnconfirmedTransactions
+    // );
+    // this.updateService.start();
   }
 
-  private handleUnconfirmedTransactions = (transactions: Transaction[]) => {
-    console.log("Found new txs: ", transactions.length);
-    for (const candidTransaction of transactions) {
-      let existing = this.mempool.find(tx => tx.id === candidTransaction.id);
-      if (existing) continue;
+  create() {
+    this.add.circle(this.dest.x, this.dest.y, 10, 0x00aaf0f);
 
-      this.addTransaction(candidTransaction);
+    let person = this.add.circle(this.start.x, this.start.y, 10, 0xff0000);
+    this.physics.add.existing(person);
+    this.personBody = person.body as any;
+    // this.physics.moveToObject(person, this.dest, this.speed);
+
+    let vel = new Phaser.Math.Vector2(
+      this.dest.x - this.start.x,
+      this.dest.y - this.start.y
+    )
+      .normalize()
+      .scale(this.speed);
+
+    this.personBody.setVelocity(vel.x, vel.y);
+
+    this.distanceText = this.add.text(10, 10, "Click to set target", {
+      color: "#00ff00"
+    });
+  }
+
+  update(_time: number, deltaTime: number) {
+    let distance = Math.Distance.BetweenPoints(this.personBody.position, this.dest);
+
+    this.distanceText.setText(`Distance: ${distance}`);
+
+    if (distance < 20) {
+      this.personBody.stop();
     }
-  };
-
-  private addTransaction(transaction: Transaction) {
-    this.mempool.push(transaction);
-
-    let node = this.add.circle(
-      Math.Between(0, +this.game.config.width),
-      Math.Between(0, +this.game.config.height),
-      10,
-      0xfc5603,
-      1
-    );
   }
+
+  // update(time, deltaTime) {
+  //   let dir = this.dest.subtract(this.personBody.position).normalize();
+  //   this.personBody.position.add(dir.scale(this.speed * deltaTime));
+  // }
+
+  // private handleUnconfirmedTransactions = (transactions: Transaction[]) => {
+  //   console.log("Found new txs: ", transactions.length);
+  //   for (const candidTransaction of transactions) {
+  //     let existing = this.mempool.find(tx => tx.id === candidTransaction.id);
+  //     if (existing) continue;
+
+  //     this.addTransaction(candidTransaction);
+  //   }
+  // };
+
+  // private addTransaction(transaction: Transaction) {
+  //   this.mempool.push(transaction);
+
+  //   let node = this.add.circle(
+  //     Math.Between(0, +this.game.config.width),
+  //     Math.Between(0, +this.game.config.height),
+  //     10,
+  //     0xfc5603,
+  //     1
+  //   );
+  // }
 }
 
 let game = new Phaser.Game({
@@ -55,7 +97,8 @@ let game = new Phaser.Game({
   physics: {
     default: "arcade",
     arcade: {
-      gravity: { x: 0, y: 200 }
+      gravity: { x: 0, y: 0 },
+      debug: true
     }
   }
 });
