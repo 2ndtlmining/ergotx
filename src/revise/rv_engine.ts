40223f39db2a4c1d20ca1df12bd0c01ee7c11d32
enix403 <fog.code000@gmail.com>
2024-06-30 11:45:37 +0500

    moves

--- src/revise/rv_engine.ts
+++ src/revise/rv_engine.ts
@@ -11,6 +11,20 @@ type Placement =
   /* Tx is in a block at the given index (from top) */
   | { type: "block"; index: number };
 
+function arePlacementsEqual(a: Placement, b: Placement) {
+  if (a.type !== b.type) return false;
+
+  switch (a.type) {
+    case "waiting":
+      return true;
+    case "block":
+      return a.index === (<any>b).index;
+
+    default:
+      return false;
+  }
+}
+
 /* ========================== */
 
 const TX_MAX_AGE = 4;
@@ -73,85 +87,128 @@ class TxStateSet {
 }
 
 class AssemblySnapshot {
-  constructor(public transactions: Transaction[], public states: TxStateSet) {}
-
-  public clone() {
-    return new AssemblySnapshot([...this.transactions], this.states.clone());
-  }
+  constructor(
+    public readonly transactions: Transaction[],
+    public readonly states: TxStateSet
+  ) {}
 }
+
+/* ====================================== */
+
+interface Move {
+  tx: Transaction;
+  placement: Placement | null;
+  isSpawning: boolean;
+  isDying: boolean;
+}
+
+function CalculateMoves(
+  snapshotA: AssemblySnapshot,
+  snapshotB: AssemblySnapshot
+) {
+  let combinedSet = PropSet.fromArray(
+    [...snapshotA.transactions, ...snapshotB.transactions],
+    tx => tx.id
+  );
+
+  let moves: Move[] = [];
+
+  for (const tx of combinedSet.getItems()) {
+    let aliveBefore = snapshotA.states.isAlive(tx);
+    let aliveNow = snapshotB.states.isAlive(tx);
+
+    let placementBefore = snapshotA.states.getState(tx)?.placement ?? null;
+    let placementAfter = snapshotB.states.getState(tx)?.placement ?? null;
+
+    if (aliveBefore && aliveNow) {
+      // check placement
+      if (!arePlacementsEqual(placementBefore!, placementAfter!)) {
+        moves.push({
+          tx,
+          placement: placementAfter,
+          isSpawning: false,
+          isDying: false,
+        });
+      }
+    } else if (aliveBefore && !aliveNow) {
+      // register destroy move
+      moves.push({
+        tx,
+        placement: null,
+        isSpawning: false,
+        isDying: true
+      });
+    } else if (!aliveBefore && aliveNow) {
+      // register 'waiting' or 'block' move depending on placement. spawns
+      moves.push({
+        tx,
+        placement: placementAfter,
+        isSpawning: true,
+        isDying: false
+      });
+    }
+  }
+
+  return moves;
+}
+
+/* ====================================== */
 
 class Engine {
   private assembly: AssemblySnapshot;
-  /*
-  targetAssembly
-  spawningTxs
-  dyingTxs
-  movingTxs
-  */
+  private targetAssembly: AssemblySnapshot | null;
+  private activeMoves: Move[];
 
   constructor() {
     // Starts with empty assembly
     this.assembly = new AssemblySnapshot([], new TxStateSet());
+    this.targetAssembly = null;
   }
 
   private onTx(incomingTx: Transaction[]) {
-    let newAssembly = this.assembly.clone();
+    let newStates = this.assembly.states.clone();
 
     // increment
-    newAssembly.states.incrementAll();
+    newStates.incrementAll();
 
     // append new or reset age of new
     for (const tx of incomingTx) {
-      newAssembly.states.markSeen(tx);
+      newStates.markSeen(tx);
     }
 
     // remove with age >= MAX_AGE
     let combinedSet = PropSet.fromArray(
-      [...newAssembly.transactions, ...incomingTx],
+      [...this.assembly.transactions, ...incomingTx],
       tx => tx.id
     );
 
     let newTransactions: Transaction[] = [];
 
     for (const tx of combinedSet.getItems()) {
-      let state = newAssembly.states.getState(tx)!;
+      let state = newStates.getState(tx)!;
       if (state.age >= TX_MAX_AGE) {
-        newAssembly.states.remove(tx);
+        newStates.remove(tx);
       } else {
         newTransactions.push(tx);
       }
     }
-
-    newAssembly.transactions = newTransactions;
 
     let assembled = new DefaultAssembleStrategy().assembleTransactions(
       newTransactions
     );
 
     for (const { tx, placement } of assembled) {
-      newAssembly.states.getState(tx)!.placement = placement;
+      newStates.getState(tx)!.placement = placement;
     }
+
+    let newAssembly = new AssemblySnapshot(newTransactions, newStates);
+    this.targetAssembly = newAssembly;
+    this.activeMoves = CalculateMoves(this.assembly, newAssembly);
   }
 }
-/*
-function CalculateMoves(snapshotA: AssemblySnapshot, snapshotB: AssemblySnapshot) {
-  let combined = [...snapshotA.transactions, ...snapshotB.transactions];
 
-  for (const tx of combined) {
-    let aliveBefore = snapshotA.states.isAlive(tx);
-    let aliveNow = snapshotB.states.isAlive(tx);
+/* ================================ */
 
-    if (aliveBefore && aliveNow) {
-      // check placement
-    }
-    else if (aliveBefore && !aliveNow) {
-      // register destroy move
-    }
-    else if (!aliveBefore && aliveNow) {
-      // register 'waiting' or 'block' move depending on placement
-    }
-  }
-} */
 
 /*
 
@@ -199,4 +256,3 @@ tick(newTxs):
 
 */
 
-/* ========================== */
