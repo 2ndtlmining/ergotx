8b6294f39d1c82860b014f0b5ddd5cca1d80308f
enix403 <fog.code000@gmail.com>
2024-07-14 17:58:16 +0500

    Block tick

--- src/engine/Engine.ts
+++ src/engine/Engine.ts
@@ -7,7 +7,7 @@ import { DefaultAssembleStrategy } from "~/assemble/DefaultAssembleStrategy";
 
 import { AssemblySnapshot, TxStateSet } from "./state-snapshot";
 import type { Command, AcceptsCommands } from "./Command";
-import { arePlacementsEqual } from "./Placement";
+import { arePlacementsEqual, Placement } from "./Placement";
 
 const TX_MAX_AGE = 4;
 
@@ -147,26 +147,99 @@ export class Engine {
       }
     }
 
-    if (lifespans.length > 0)
-      await this.cmdExecutor.executeCommands(lifespans);
+    if (lifespans.length > 0) await this.cmdExecutor.executeCommands(lifespans);
 
-    if (walks.length > 0)
-      await this.cmdExecutor.executeCommands(walks);
+    if (walks.length > 0) await this.cmdExecutor.executeCommands(walks);
   }
 
   private async startTxsTick(incomingTxs: Transaction[]) {
     let newStates = this.assembly.states.clone();
     let newTransactions = this.mergeIncomingTxs(newStates, incomingTxs);
-    this.assembleTxs(newStates, newTransactions);
 
+    this.assembleTxs(newStates, newTransactions);
     this.targetAssembly = new AssemblySnapshot(newTransactions, newStates);
 
     // Commands
     return this.emitTxsCommands();
   }
 
+  /* ================= BLOCK TICK ================= */
+
   private async startBlockTick(block: TransactedBlock) {
-    return Promise.resolve();
+    let newStates = this.assembly.states.clone();
+    let remainingTransactions: Transaction[] = [];
+
+    for (const tx of this.assembly.transactions) {
+      // If this transaction is also part of the (found and
+      // outgoing) block then remove it
+      if (block.transactions.find(blockTx => blockTx.id === tx.id)) {
+        newStates.remove(tx);
+      }
+      // Otherwise store it for reassembly
+      else {
+        remainingTransactions.push(tx);
+      }
+    }
+
+    // Calculate target assembly
+    this.assembleTxs(newStates, remainingTransactions);
+    this.targetAssembly = new AssemblySnapshot(
+      remainingTransactions,
+      newStates
+    );
+
+    let spawns: Command[] = [];
+    let walks: Command[] = [];
+
+    for (const tx of block.transactions) {
+      let placementBefore = this.assembly.states.getState(tx)?.placement;
+      let placementAfter: Placement = { type: "block", index: 0 };
+
+      // If the transaction did not exist in the last snapshot
+      if (!placementBefore) {
+        // First spawn and the move to top block
+        spawns.push({ type: "spawn", tx });
+        walks.push({
+          type: "walk",
+          tx,
+          placement: placementAfter
+        });
+      }
+      // Otherwise (placement did exist before) check if it was
+      // anywhere other than top block. If so, then move it to top block
+      else if (!arePlacementsEqual(placementBefore, placementAfter)) {
+        walks.push({
+          type: "walk",
+          tx,
+          placement: placementAfter
+        });
+      }
+    }
+
+    for (const tx of remainingTransactions) {
+      let placementBefore = this.assembly.states.getState(tx)!.placement;
+      let placementAfter = structuredClone(
+        this.targetAssembly.states.getState(tx)!.placement
+      );
+
+      if (placementAfter.type === "block")
+        //
+        placementAfter.index += 1;
+
+      if (!arePlacementsEqual(placementBefore, placementAfter)) {
+        walks.push({
+          type: "walk",
+          tx,
+          placement: placementAfter
+        });
+      }
+    }
+
+    if (spawns)
+      await this.cmdExecutor.executeCommands(spawns);
+
+    if (walks)
+      await this.cmdExecutor.executeCommands(walks);
   }
 
   public update() {
@@ -182,7 +255,9 @@ export class Engine {
           break;
 
         case "block":
-          tickPromise = this.startBlockTick(nextUpdate.block);
+          console.log("Found block", nextUpdate.block);
+          // tickPromise = this.startBlockTick(nextUpdate.block);
+          tickPromise = Promise.resolve();
           break;
       }
 
