8d5f4a50fa423098dde71caa2ae4746252b94d7d
enix403 <fog.code000@gmail.com>
2024-07-21 18:55:08 +0500

    Refactor

--- src/rendering/actors/LiveBus.ts
+++ src/rendering/actors/LiveBus.ts
@@ -5,38 +5,42 @@ import { Actor } from "./Actor";
 import { Transform } from "~/common/component-types";
 
 export class LiveBus extends Actor implements SupportsMotion {
-  private gameObject: GameObjects.Image;
-  private motionController: MotionController;
+  private container: GameObjects.Container;
+  private sprite: GameObjects.Image;
+  private height: number;
 
+  private motionController: MotionController;
   constructor(scene: Scene, width: number) {
     super(scene);
 
     {
-      let image = this.scene.add.image(-1000, -1000, "plane");
-      image.scale = width / image.width;
-      image.setOrigin(0.5, 0);
-      this.gameObject = image;
+      let sprite = this.sprite = this.scene.add.image(0, 0, "plane");
+      sprite.scale = width / sprite.width;
+      sprite.setOrigin(0.5, 0);
+      this.height = sprite.getBounds().height;
+
+      this.container = scene.add.container(-1000, -1000);
+      this.container.add(sprite);
     }
 
-    this.gameObject.depth = 1;
+    this.container.depth = 1;
 
-    this.motionController = new MotionController(this.gameObject);
+    this.motionController = new MotionController(this.container);
   }
 
   public getHeight() {
-    return this.gameObject.getBounds().height;
+    return this.height;
   }
 
   public place(position: IVector2) {
-    this.gameObject.copyPosition(position);
+    this.container.copyPosition(position);
   }
 
   public getWalkInTargetPoint() {
     // TODO: cache region (and update when moved) for faster
     // point calculation
-    let allowedRegion = this.gameObject.getBounds();
+    let allowedRegion = this.sprite.getBounds();
 
-    // TODO: inflate according to width and/or height
     Geom.Rectangle.Inflate(allowedRegion, -100, -100);
     return allowedRegion.getRandomPoint();
   }
@@ -48,11 +52,11 @@ export class LiveBus extends Actor implements SupportsMotion {
   }
 
   public destroy() {
-    this.gameObject.destroy();
+    this.container.destroy();
   }
 
   public getTransform(): Transform {
-    return this.gameObject;
+    return this.container;
   }
 
   public getMotionController(): MotionController {
