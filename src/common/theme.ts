9ad5dfb22a90dc1e4cc0f2770baecd98a990429c
enix403 <fog.code000@gmail.com>
2024-07-21 16:40:24 +0500

    Tweaks

--- src/common/theme.ts
+++ src/common/theme.ts
@@ -4,8 +4,8 @@ export const WHITE = 0xffffff;
 export const SCENE_BG_COLOR = 0x313131;
 export const TILE_GRIDLINES_COLOR = 0x0457db;
 
-export const PERSON_RADIUS = 20;
-export const PERSON_COLOR = 0xedae26;
+export const PERSON_RADIUS = 10;
+export const PERSON_COLOR = 0x6c15ed;
 
 export const HOUSE_RADIUS = 6;
 export const HOUSE_COLOR = WHITE;
