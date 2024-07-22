c6db5042814047a79a649f8645e4d2aea8289550
enix403 <fog.code000@gmail.com>
2024-07-22 17:33:24 +0500

    AgeMap

--- src/engine/AgeMap.ts
+++ src/engine/AgeMap.ts
@@ -1,8 +1,7 @@
 import { TX_MAX_AGE } from "~/common/constants";
 import { Transaction } from "~/common/types";
 
-// TODO: Is a wrapper with 1-to-1 API mapping really required ?
-class AgeMap {
+export class AgeMap {
   private map: Map<string, number>;
 
   constructor() {
@@ -24,38 +23,59 @@ class AgeMap {
   public remove(txId: string) {
     this.map.delete(txId);
   }
-}
 
-export function ExtendAgeMap(
-  prevMap: AgeMap,
-  prevTransactions: Transaction[],
-  newTransactions: Transaction[]
-) : [AgeMap, Transaction[]] {
-  let newMap = new AgeMap();
+  public extended(
+    currentTransactions: Transaction[],
+    newTransactions: Transaction[]
+  ): [AgeMap, Transaction[]] {
+    let newMap = new AgeMap();
+    let includedTransactions: Transaction[] = [];
 
-  let includedTransactions: Transaction[] = [];
-
-  for (const tx of newTransactions) {
-    newMap.set(tx.id, 1);
-    includedTransactions.push(tx);
-  }
-
-  for (const tx of prevTransactions) {
-    if (newMap.has(tx.id)) {
-      // Ignore if the transaction is one of the new transactions (and
-      // hence already has been accounted for in the above loop)
-      continue;
-    }
-
-    // Otherwise include it only if it is not expired
-    let newAge = prevMap.get(tx.id) + 1;
-
-    if (newAge <= TX_MAX_AGE) {
-      newMap.set(tx.id, newAge);
+    for (const tx of newTransactions) {
+      newMap.set(tx.id, 1);
       includedTransactions.push(tx);
     }
+
+    for (const tx of currentTransactions) {
+      if (newMap.has(tx.id)) {
+        // Ignore if the transaction is one of the new transactions (and
+        // hence already has been accounted for in the above loop)
+        continue;
+      }
+
+      // Otherwise include it only if it is not expired
+      let newAge = this.get(tx.id) + 1;
+
+      if (newAge <= TX_MAX_AGE) {
+        newMap.set(tx.id, newAge);
+        includedTransactions.push(tx);
+      }
+    }
+
+    return [newMap, includedTransactions];
   }
 
-  return [newMap, includedTransactions];
-}
+  public shrinked(
+    currentTransactions: Transaction[],
+    removeTransactions: Transaction[]
+  ): [AgeMap, Transaction[]] {
+    let newMap = new AgeMap();
+    let includedTransactions: Transaction[] = [];
 
+    for (const tx of removeTransactions) {
+      newMap.set(tx.id, -1);
+    }
+
+    for (const tx of currentTransactions) {
+      if (newMap.has(tx.id)) {
+        newMap.remove(tx.id);
+        continue;
+      }
+
+      newMap.set(tx.id, this.get(tx.id));
+      includedTransactions.push(tx);
+    }
+
+    return [newMap, includedTransactions];
+  }
+}
