c825da252df69754b4b063831f471e3367a62ced
enix403 <fog.code000@gmail.com>
2024-07-28 01:26:38 +0500

    change update service

--- src/engine/Engine.ts
+++ src/engine/Engine.ts
@@ -33,8 +33,6 @@ export class Engine {
     this.isPaused = false;
 
     this.updateService = updateService;
-    // this.updateService = new PollUpdateService();
-    // this.updateService = new ReplayUpdateService("/replays/replay-01.json");
 
     if (startUpdates) {
       updateService.start();
