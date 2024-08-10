25235bd5d2ced23fe0119a17b41ea95350ddbd9f
enix403 <fog.code000@gmail.com>
2024-08-11 01:47:01 +0500

    Updated LinearMotion to use app's Vector

--- src/movement/LinearMotion.ts
+++ src/movement/LinearMotion.ts
@@ -1,5 +1,4 @@
-import { Math } from "phaser";
-import { IVector2 } from "~/math/vector";
+import { Vector, ThinVector } from "~/math/vector";
 import { Time } from "~/scene/Time";
 
 import { Motion } from "./motion";
@@ -8,34 +7,24 @@ const SPEED = 0.45;
 const MAX_DELTA_DIST = 2;
 
 export class LinearMotion extends Motion {
-  private lastPoint: Math.Vector2;
   private nextPointIndex: number;
 
-  constructor(public readonly points: IVector2[]) {
+  constructor(public readonly points: ThinVector[]) {
     super();
-    this.lastPoint = new Math.Vector2();
     this.nextPointIndex = -1;
   }
 
   public _init() {
-    this.lastPoint.set(
-      this.controller.gameObject.x,
-      this.controller.gameObject.y
-    );
     this.nextPointIndex = 0;
   }
 
   public _update(): void {
-    let source = this.controller.gameObject as IVector2;
+    let source = Vector.fromObject(this.controller.gameObject);
     let destination = this.points[this.nextPointIndex];
 
-    let direction = new Math.Vector2(
-      destination.x - source.x,
-      destination.y - source.y
-    );
-
+    let direction = Vector.fromObject(destination).subIp(source);
     let distToTarget = direction.length();
-
+    
     let displacementLength = SPEED * Time.DeltaTime;
 
     if (distToTarget <= MAX_DELTA_DIST || displacementLength > distToTarget) {
@@ -50,14 +39,8 @@ export class LinearMotion extends Motion {
         return;
       }
     } else {
-      let displacement = direction
-        .clone()
-        .normalize()
-        .scale(displacementLength);
-
-      let nextPoint = new Math.Vector2()
-        .setFromObject(source)
-        .add(displacement);
+      let displacement = direction.normalize().mul(displacementLength);
+      let nextPoint = source.add(displacement);
 
       this.controller.gameObject.setX(nextPoint.x);
       this.controller.gameObject.setY(nextPoint.y);
