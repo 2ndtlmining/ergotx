f738efd16b17d6c8e02c1bdf33d8c11c6fe37866
enix403 <fog.code000@gmail.com>
2024-08-16 19:30:39 +0500

    save

--- src/pages/vis-block/BlockVisualizer.svelte
+++ src/pages/vis-block/BlockVisualizer.svelte
@@ -1,12 +1,14 @@
 <script lang="ts">
   import { clearIntervalAsync, setIntervalAsync } from 'set-interval-async';
   import { onMount } from 'svelte';
-  import { getBlocks, getBlocksAbove, getNetworkStats } from '~/ergoapi/apiconn'; 
+  import { getBlocks, getBlocksAbove, getNetworkStats } from '~/ergoapi/apiconn';
   import type { Block } from '~/types/ergo';
   import { formatErg, formatNumber, parseNumber } from '~/utils/number';
-  
+
   import { calculateScores, type Score } from './scores';
-  
+  import { IconCpu, IconCube } from '@tabler/icons-svelte';
+  import clsx from 'clsx';
+
   // latest block at the end
   let blocks: Block[] = [];
   let lastBlockSeenTime = -1;
@@ -15,17 +17,17 @@
     difficulty: 0,
     hashRate: 0,
   }
-  
+
   async function fetchLastDayBlocks() {
-    let chunk1Promise = getBlocks(500, 0, "height", "desc");    
+    let chunk1Promise = getBlocks(500, 0, "height", "desc");
     let chunk2Promise = getBlocks(220, 500, "height", "desc");
-    
+
     let [chunk1, chunk2] = await Promise.all([chunk1Promise, chunk2Promise]);
-    
+
     let allBlocks = [...chunk1, ...chunk2];
-    
+
     allBlocks.reverse();
-    
+
     return allBlocks;
   }
 
@@ -33,26 +35,26 @@
     if (blocks.length === 0) {
       return await fetchLastDayBlocks();
     }
-    
+
     let lastConfirmedHeight = blocks[blocks.length - 1].height;
     return await getBlocksAbove(lastConfirmedHeight);
   }
-  
+
   function updateBlocks() {
     fetchNewBlocks().then((newBlocks) => {
-      
+
       if (newBlocks.length > 0) {
         lastBlockSeenTime = new Date().getTime();
       }
-      
+
       let totalBlocks = blocks.length + newBlocks.length;
       let extra = Math.max(0, totalBlocks - 720);
-      
+
       blocks = [...blocks, ...newBlocks].slice(extra);
       // console.log(blocks);
     });
   }
-  
+
   function updateTimeSinceLastBlock() {
     let now = new Date().getTime();
 
@@ -61,41 +63,72 @@
       ? "N/A"
       : formatNumber((now - lastBlockSeenTime) / 1000, { mantissa: 0 }) +
       " seconds ago";
-      
+
     timeSinceLastBlock = formatted;
   }
-  
+
   onMount(() => {
+    return;
     getNetworkStats().then(netStats => {
       stats.hashRate = parseNumber(netStats?.['miningCost']?.['hashRate'], 0);
       stats.difficulty = parseNumber(netStats?.['miningCost']?.['difficulty'], 0);
     });
-    
+
     // Block fetcher
     updateBlocks();
     let blockFetcherId = setIntervalAsync(updateBlocks, 2 * 60 * 1000); // every 2 minutes
-    
+
     // Block timer
     updateTimeSinceLastBlock();
     let blockTimerId = setInterval(updateTimeSinceLastBlock, 1000) // every second;
-    
+
     return () => {
       clearIntervalAsync(blockFetcherId);
       clearInterval(blockTimerId);
     }
   });
-  
+
   // =====================
 
   let scores: Score[] = [];
 
   $: scores = calculateScores(blocks);
-  
+
 </script>
 
 <div class="p-4 overflow-auto w-full">
-  <div class="flex w-full overflow-x-auto">
-    <table class="table-compact table-zebra table max-w-2xl">
+  <div class="flex gap-x-8 max-w-3xl">
+    <div class={clsx(
+      "flex flex-col justify-center items-center gap-y-4",
+      "bg-[#1f1f1f] shadow-lg shadow-[#1c1b1b] rounded-lg",
+      "px-10 py-6 flex-1"
+    )}>
+      <IconCube size={64} />
+      <p>Time Since <br /> Last Block</p>
+      <p>8.45 TH/s</p>
+    </div>
+    <div class={clsx(
+      "flex flex-col justify-center items-center gap-y-4",
+      "bg-[#1f1f1f] shadow-lg shadow-[#1c1b1b] rounded-lg",
+      "px-10 py-6 flex-1"
+    )}>
+      <IconCube size={64} />
+      <p>Hash Rate</p>
+      <p>8.45 TH/s</p>
+    </div>
+    <div class={clsx(
+      "flex flex-col justify-center items-center gap-y-4",
+      "bg-[#1f1f1f] shadow-lg shadow-[#1c1b1b] rounded-lg",
+      "px-10 py-6 flex-1"
+    )}>
+      <IconCube size={64} />
+      <p>Hash Rate</p>
+      <p>8.45 TH/s</p>
+    </div>
+  </div>
+
+  <div class="flex w-full overflow-x-auto mt-10">
+    <table class="table-compact table-zebra table max-w-3xl">
       <thead>
         <tr>
           <th>Spot</th>
