47f5ad53a121d365d9f1b1c49a56070ae19f772b
enix403 <fog.code000@gmail.com>
2024-08-17 13:18:41 +0500

    Stat box

--- src/pages/vis-tx/renderer/actors/StatsDisplay.ts
+++ src/pages/vis-tx/renderer/actors/StatsDisplay.ts
@@ -1,23 +1,72 @@
 import { GameObjects, Scene } from "phaser";
 import { pixels } from "../sizing";
 
-// const center = ()
-
 class StatBox {
+  private rect: GameObjects.Rectangle;
+  private labelText: GameObjects.Text;
+  private valueText: GameObjects.Text;
+
+  private padX: number;
+  private inX: number;
+  private inWidth: number;
+
   constructor(
     scene: Scene,
+    label: string,
+    value: string,
     x: number,
     y: number,
     width: number,
     height: number
-  ) {}
+  ) {
+    this.padX = 20;
+    this.inX = x + this.padX;
+    this.inWidth = width - 2 * this.padX;
+
+    let surface = scene.add.rectangle(x, y, width, height);
+    surface.setOrigin(0, 0);
+    surface.setFillStyle(0xc2c3c4);
+    surface.setDepth(3);
+
+    let rect = scene.add.rectangle(this.inX, y, this.inWidth, height);
+    this.rect = rect;
+    rect.setOrigin(0, 0);
+    rect.setFillStyle(0x212121);
+    rect.setDepth(3);
+
+    let textStyle = {
+      fontFamily: "Minecraft",
+      color: "#D77F0E",
+      fontSize: 28
+    };
+
+    this.labelText = scene.add
+      .text(this.inX, y + 25, label, textStyle)
+      .setOrigin(0, 0)
+      .setDepth(3);
+
+    this.valueText = scene.add.text(this.inX, y + 65, value, textStyle).setDepth(3);
+
+    this.center(this.labelText);
+    this.center(this.valueText);
+  }
+
+  private center(text: GameObjects.Text) {
+    text.setX(this.inX + (this.inWidth - text.width) / 2);
+  }
+
+  public setValue(value: string) {
+    this.valueText.setText(value);
+    this.center(this.valueText);
+  }
 }
 
 export class StatsDisplay {
-  private rect: GameObjects.Rectangle;
-
   public blockTimeText: GameObjects.Text;
   public mempoolSizeText: GameObjects.Text;
+
+  private blockTimeBox: StatBox;
+  private pendingTxBox: StatBox;
 
   constructor(
     scene: Scene,
@@ -27,86 +76,16 @@ export class StatsDisplay {
     height: number
   ) {
     this.temp(scene);
-    this.initBlockTimeDisplay(scene);
-    this.initPendingTxDisplay(scene);
-  }
-
-  private initBlockTimeDisplay(scene: Phaser.Scene) {
-    let width = 300;
-    let height = 125;
-    let padX = 20;
-
-    let x = 50;
-    let y = 10;
-
-    let inX = x + padX;
-    let inWidth = width - 2 * padX;
-
-    let surface = scene.add.rectangle(x, y, width, height);
-    surface.setOrigin(0, 0);
-    surface.setFillStyle(0xc2c3c4);
-    surface.setDepth(3);
-
-    let rect = scene.add.rectangle(inX, y, inWidth, height);
-    rect.setOrigin(0, 0);
-    rect.setFillStyle(0x212121);
-    rect.setDepth(3);
-
-    let textStyle = {
-      fontFamily: "Minecraft",
-      color: "#D77F0E",
-      fontSize: 28
-    };
-
-    let line1 = scene.add
-      .text(0, y + 25, "Last Block In", textStyle)
-      .setOrigin(0, 0)
-      .setDepth(3);
-
-    let line2 = scene.add
-      .text(0, y + 65, "01 min 02 sec", textStyle)
-      .setDepth(3);
-
-    line1.setX(inX + (inWidth - line1.width) / 2);
-    line2.setX(inX + (inWidth - line2.width) / 2);
-  }
-
-  private initPendingTxDisplay(scene: Phaser.Scene) {
-    let width = 300;
-    let height = 125;
-    let padX = 20;
-
-    let x = 500;
-    let y = 10;
-
-    let inX = x + padX;
-    let inWidth = width - 2 * padX;
-
-    let surface = scene.add.rectangle(x, y, width, height);
-    surface.setOrigin(0, 0);
-    surface.setFillStyle(0xc2c3c4);
-    surface.setDepth(3);
-
-    let rect = scene.add.rectangle(inX, y, inWidth, height);
-    rect.setOrigin(0, 0);
-    rect.setFillStyle(0x212121);
-    rect.setDepth(3);
-
-    let textStyle = {
-      fontFamily: "Minecraft",
-      color: "#D77F0E",
-      fontSize: 28
-    };
-
-    let line1 = scene.add
-      .text(0, y + 25, "Pending Txs", textStyle)
-      .setOrigin(0, 0)
-      .setDepth(3);
-
-    let line2 = scene.add.text(0, y + 65, "32", textStyle).setDepth(3);
-
-    line1.setX(inX + (inWidth - line1.width) / 2);
-    line2.setX(inX + (inWidth - line2.width) / 2);
+    this.blockTimeBox = new StatBox(
+      scene,
+      "Last Block In", "00 min 00 sec",
+      50, 10, 300, 125
+    );
+    this.pendingTxBox = new StatBox(
+      scene,
+      "Pending Txs", "32",
+      500, 10, 300, 125
+    );
   }
 
   private temp(scene: any) {
