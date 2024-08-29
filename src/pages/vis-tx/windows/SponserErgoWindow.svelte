79043ce39b60ad6047bb85f2f5b0d510af7dc5c5
enix403 <fog.code000@gmail.com>
2024-08-29 18:57:43 +0500

    Add confetti

--- src/pages/vis-tx/windows/SponserErgoWindow.svelte
+++ src/pages/vis-tx/windows/SponserErgoWindow.svelte
@@ -1,8 +1,9 @@
 <script>
   import { IconTrophyFilled } from "@tabler/icons-svelte";
+  import { Confetti } from "svelte-confetti"
 </script>
 
-<main>
+<main class="relative">
   <div class="flex flex-col items-center px-4">
     <div class="text-[#09b573] p-4 bg-[#09b573]/30 rounded-full">
       <IconTrophyFilled size={70} />
@@ -16,5 +17,18 @@
       Your support means the world to us. If you'd like to contribute, please donate to the following Ergo address:
     </p>
   </div>
+  
+  <div class="absolute -top-8 left-0 right-0 h-32">
+    <Confetti
+      infinite
+      y={[0, 0.1]}
+      x={[-5, 5]}
+      amount={100}
+      fallDistance="170px"
+      duration={1700}
+      size={15}
+    /> 
+  </div>
+  
 </main>
 
