5b938e7abc73acc56a5178c39e8153d5c7022a13
2nTL <2ndtlmining@gmail.com>
2024-09-02 08:23:17 +1000

    small updates

--- conf/contact.ts
+++ conf/contact.ts
@@ -1,4 +1,4 @@
 export const facebook = "https://www.facebook.com/your-facebook-page";
-export const youtube = "https://www.youtube.com/your-youtube-page";
-export const email = "your@email.com";
-export const github = "https://www.github.com/your-github";
+export const youtube = "https://www.youtube.com/@2ndtlmining";
+export const email = "2ndtlmining@gmail.com";
+export const github = "https://github.com/2ndtlmining/ergotx";
